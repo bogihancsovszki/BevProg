75e7869b85859d32f75f1d89025129d4fe7dd126
bogihancsovszki <hancsovszki33@gmail.com>
2021-10-15 23:08:38 +0200

    drill5

---
+++ source/drill5/5/5.cpp
@@ -0,0 +1,25 @@
+#include "std_lib_facilities.h"
+
+int main()
+try {
+    
+
+    int res = 7; /* string res = 7; -> int tipusu a változó     */
+    vector<int> v(10); 
+    v[5] = res; 
+    cout << "Success!\n";
+
+
+    keep_window_open();
+    return 0;
+}
+catch (exception& e) {
+    cerr << "error: " << e.what() << '\n';
+    keep_window_open();
+    return 1;
+}
+catch (...) {
+    cerr << "Oops: unknown exception!\n";
+    keep_window_open();
+    return 2;
+}
---
+++ source/drill5/7/7.cpp
@@ -0,0 +1,25 @@
+#include "std_lib_facilities.h"
+
+int main()
+try {
+    
+    bool cend  = true; 
+    if ( cend )
+        cout << "Success!\n"; 
+    
+    else cout << "Fail!\n";
+
+
+    keep_window_open();
+    return 0;
+}
+catch (exception& e) {
+    cerr << "error: " << e.what() << '\n';
+    keep_window_open();
+    return 1;
+}
+catch (...) {
+    cerr << "Oops: unknown exception!\n";
+    keep_window_open();
+    return 2;
+}
